fa1e2cd6899f12ae1e5196e3b62c0981b81f2c0c
Frank Robnik <frank.robnik@gmail.com>
2021-08-06 18:56:30 +0200

    add test folder for mocha/chai

---
+++ test/test.js
@@ -0,0 +1,16 @@
+var should = require('chai').should();
+
+const simpleGraph = require('../SimpleGraph.js');
+
+console.log('test something');
+
+
+describe('Circular detector', function () {
+
+  it('should add vertices to graph', function() {});
+
+  it('should remove vertices from graph', function() {});
+  
+  it('should check if a and b are connected or not', function() {});
+ 
+});
