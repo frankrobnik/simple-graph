d5af9e96d5a2084bb7f19b443fafc7cdc5524bd3
Frank Robnik <frank.robnik@gmail.com>
2021-08-11 21:02:30 +0200

    update test file

--- test/test.js
+++ test/test.js
@@ -8,19 +8,31 @@ describe('Circular detector', function () {
   
   beforeEach(function () {
     graph = new SimpleGraph();
-    graph.addVertex(['A', 'B', 'C', 'D', 'E']);
+    graph.addVertex('A', 'B', 'C', 'D', 'E');
     graph.addVertex('X');
     graph.addEdge('A','B');
     graph.addEdge('A','D');
     graph.addEdge('B','C');
     graph.addEdge('C','D');
+    graph.addEdge('C','E');
     graph.addEdge('D','E');
   });
 
+  // missing Tests:
+  // addVertex
+  // removeVertex
+  // addEdge
+  // addEdge undirected
+  // removeEdge
+  // removeEdge undirected
+
   it('should check if two nodes are connected or not.', function() {
-    graph.connected('A','B').should.be.true;
-    graph.connected('A','C').should.be.true;
-    graph.connected('B','X').should.be.false;
+    graph.isConnected('A','B').should.be.true;
+    graph.isConnected('A','C').should.be.true;
+    graph.isConnected('B','X').should.be.false;
+    graph.addEdge('E','X');
+    graph.isConnected('B','X').should.be.true;
   });
  
+
 });
