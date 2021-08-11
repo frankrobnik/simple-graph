8b1dba629ae48e8b87759745ad4279eb7390962d
Frank Robnik <frank.robnik@gmail.com>
2021-08-11 20:19:29 +0200

    move SimpleGraph.ts to src folder

---
+++ src/SimpleGraph.ts
@@ -0,0 +1,58 @@
+class SimpleGraph {
+  private _graph: Map<string, Set<string>>;
+  
+  constructor () {
+    this._graph = new Map();  
+  }
+
+  addVertex (key: string | Array<string>):boolean {
+    if (typeof key === 'string') this._graph.set(key, new Set());
+    else key.forEach(key => this._graph.set(key, new Set()));
+    return true;
+  }
+
+  removeVertex (key: string):boolean {
+    this._graph.get(key).forEach(adjacent => this.removeEdge(adjacent, key) );
+    this._graph.delete(key);
+    return true;
+  }
+
+  addEdge (from:string, to:string, directed:boolean = true):boolean {
+    if(from === to) throw new Error('From and to cannot be the same.');
+    this._graph.get(from).add(to);
+    if (!directed) this._graph.get(to).add(from);
+    return true;
+  }
+  
+  removeEdge (from:string, to:string):boolean {
+    this._graph.get(from).delete(to);
+    return true;
+  }
+
+  connected (start:string, end:string):boolean {
+    if(!this._graph.has(start)) throw new Error(`Graph does not contain '${start}'.`);
+    if(!this._graph.has(end)) throw new Error(`Graph does not contain '${end}'.`);
+    if(start === end) throw new Error(`'start' and 'end' cannot be the same.`);
+    
+    const queue:Array<string> = [];
+    const visited:Array<string> = [];
+
+    const search = (vertex:string):boolean => {
+      const currentVertex = this._graph.get(vertex);
+      
+      if (currentVertex.has(end)) return true;
+      else {
+        visited.push(vertex);
+        currentVertex.forEach((edge:string) => !visited.includes(edge) && queue.push(edge));
+        if (queue.length) return search(queue.shift());
+      }
+      return false;
+    }
+
+    return search(start);
+  }
+
+}
+
+
+module.exports = SimpleGraph;
